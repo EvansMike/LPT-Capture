92988d06e7bb37884dea6ccd4c9fee900d8a4e83
Mike Evans <mikee@saxicola.co.uk>
2020-12-23 23:45:21 +0000

    Convert to nano/mega168.

--- src/main.c
+++ src/main.c
@@ -6,12 +6,12 @@
  * Wiring Layout
  * -------------
  *
- * Parallel Port Output               AVR Pins
+ * Parallel Port Output               Mega168 Pins                  UNO pins
  * --------------------               -------------
  * Name      Dir.   Pin                Name     Pin
  * ----      ----   ---                ----     ---
- * nSTROBE    >       1................INT0     PB2 (as interrupt)
- * DATA BYTE  >     2-9....................     PORTA
+ * nSTROBE    >       1................INT0     PD2 (INT0)
+ * DATA BYTE  >     2-9....................
  * nACK       <      10....................     3
  * BUSY       <      11....................     4
  * OutofPaper <      12................GND
@@ -36,21 +36,13 @@
 #include <avr/pgmspace.h>
 #include <avr/eeprom.h>
 #include "gitversion.h"
-#include "serial.h"
+//#include "serial.h"
 
-// connect these to an 8 bit port
-#define Data0   5
-#define Data1   6
-#define Data2   14
-#define Data3   15
-#define Data4   16
-#define Data5   17
-#define Data6   18
-#define Data7   19
 
-#define nStrobe PB2 // To INT0 pin
-#define nAck    PB0 //
-#define busy    PB1 //
+#define nStrobe PD2 // To INT0 pin
+#define nAck    PC0 //
+#define busy    PC1 //
+#define LED     PB5
 
 enum States {
   READY,
@@ -60,11 +52,32 @@ enum States {
 } state;
 
 
+static void USART_Init (unsigned int baud)
+{
+/* Set baud rate */
+UBRR0H = (unsigned char)(baud>>8);
+UBRR0L = (unsigned char)baud;
+/* Enable receiver and transmitter */
+UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+/* Set frame format: 8data, 2stop bit */
+UCSR0C = (1<<USBS0)|(3<<UCSZ00);
+}
+
+
+static void USART_Transmit (unsigned char data)
+{
+/* Wait for empty transmit buffer */
+while (!(UCSR0A & (1<<UDRE0)))
+;
+/* Put data into buffer, sends the data */
+UDR0 = data;
+}
+
 
 /***********************************************************************
 Interrupt to detect nStrobe
  **********************************************************************/
-ISR(EXT_INT0_vect)
+ISR(INT0_vect)
 {
     state = BUSY;
 }
@@ -76,13 +89,13 @@ ISR(EXT_INT0_vect)
  **********************************************************************/
 int main()
 {
-    DDRB |= _BV(nAck) | _BV(busy);
+    DDRC |= _BV(nAck) | _BV(busy);
     char data;
-    UART_init();
+    USART_Init (9600);
     state = READY;
-    GIMSK |= _BV(INT0);
-    MCUCR |= _BV(ISC01); // Trigger on falling edge
-    PORTB |= _BV(nStrobe) | _BV(busy); // Pull these UP
+    EIMSK |= _BV(INT0);
+    EICRA |= _BV(ISC11); // Trigger on falling edge
+    PORTC |= _BV(nStrobe) | _BV(busy); // Pull these UP
     sei();
 
     while(1)
@@ -90,22 +103,24 @@ int main()
         switch (state) {
         case READY:
           //digitalWrite(Busy, LOW);
-          PORTB &= ~_BV(busy);
+          PORTC &= ~_BV(busy);
           //digitalWrite(nAck,HIGH);
-          PORTB |= _BV(nAck);
+          PORTC |= _BV(nAck);
           //digitalWrite(led, HIGH);
+          PORTB |= _BV(LED); // ON
           break;
         case BUSY: // nStrobe signal received by interrupt handler
           //digitalWrite(Busy, HIGH);
-          PORTB |= _BV(busy);
+          PORTC |= _BV(busy);
           //digitalWrite(led, LOW);
-          data = PINA;
-          UART_tx(data);
+          PORTB &= ~_BV(LED); // OFF
+          data = (PIND & 0b11111100) & (PINB & 0b00000111);
+          //UART_tx(data);
           state = ACK;
           break;
         case ACK:
           //digitalWrite(nAck,LOW);
-          PORTB &= ~_BV(nAck);
+          PORTC &= ~_BV(nAck);
           _delay_ms(5); //milliseconds. Specification minimum = 5 us
           state = READY;
           break;
